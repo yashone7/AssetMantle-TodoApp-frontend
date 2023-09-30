66df905acaebbef52eef62ba3de8a7ce173b7c53
Yashwanth Somayajula <yashwanth.somayajula16@gmail.com>
2023-09-30 20:39:57 +0530

    implemented auth routes

--- src/router.jsx
+++ src/router.jsx
@@ -5,12 +5,14 @@ import {
 } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Login from "./Components/Auth/Login";
+import Signup from "./Components/Auth/Signup";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<Home />} errorElement={<h1>Not found</h1>} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </>
   )
 );
