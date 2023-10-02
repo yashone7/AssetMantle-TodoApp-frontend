4019f516b9aad42fcda85812f264b1692e328d6d
Yashwanth Somayajula <yashwanth.somayajula16@gmail.com>
2023-10-03 00:05:05 +0530

    modified fetcher to use headers

--- src/utils.js
+++ src/utils.js
@@ -2,5 +2,9 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-export const fetcher = (key) =>
-  axios.get(`${API_URL}/${key}`).then((res) => res.data);
+export const fetcher = ([key, token]) =>
+  axios
+    .get(`${API_URL}/api/${key}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then((res) => res.data);
