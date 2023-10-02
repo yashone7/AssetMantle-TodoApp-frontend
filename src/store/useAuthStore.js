679b1272a7f2d0b353de11197b6182680a5cc70d
Yashwanth Somayajula <yashwanth.somayajula16@gmail.com>
2023-10-03 00:03:06 +0530

    Implemented persist middleware in zustand

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -1,6 +1,16 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-export const useAuthStore = create((set, get) => ({
-  user: {},
-  setUser: (userObj) => set((state) => ({ user: userObj })),
-}));
+export const useAuthStore = create(
+  persist(
+    (set, get) => ({
+      user: {},
+      setUser: (userObj) => set((state) => ({ user: userObj })),
+      getUser: () => get().user,
+    }),
+    {
+      name: "auth-store",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
--- src/store/useBearStore.js
+++ src/store/useBearStore.js
@@ -1,7 +1,15 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-export const useBearStore = create((set, get) => ({
-  bears: 0,
-  addBears: () => set((state) => ({ bears: state.bears + 1 })),
-  removeBears: () => set((state) => ({ bears: state.bears - 1 })),
-}));
+export const useBearStore = create(
+  persist(
+    (set, get) => ({
+      bears: 0,
+      addABear: () => set({ bears: get().bears + 1 }),
+    }),
+    {
+      name: "food-storage", // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
+    }
+  )
+);
