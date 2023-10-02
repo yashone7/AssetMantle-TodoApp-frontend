4ca1fc1cd2e09ea104a9e9fb031dac796cc04bdb
Yashwanth Somayajula <yashwanth.somayajula16@gmail.com>
2023-10-03 00:04:30 +0530

    [REFACTOR] : router modified to use protcted and private routes

--- src/router.jsx
+++ src/router.jsx
@@ -6,13 +6,29 @@ import {
 import Home from "./Components/Home/Home";
 import Login from "./Components/Auth/Login";
 import Signup from "./Components/Auth/Signup";
+import App from "./App";
+import ProtectedRoute from "./Components/ProtectedRoute";
+import PublicRoute from "./Components/PublicRoute";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<Home />} errorElement={<h1>Not found</h1>} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route
+        path="/"
+        element={<PublicRoute />}
+        errorElement={<h1>Not found</h1>}
+      >
+        <Route exact path="/" element={<Home />} />
+      </Route>
+      <Route path="/login" element={<PublicRoute />}>
+        <Route exact path="/login" element={<Login />} />
+      </Route>
+      <Route path="/signup" element={<PublicRoute />}>
+        <Route exact path="/signup" element={<Signup />} />
+      </Route>
+      <Route path="/app" element={<ProtectedRoute />}>
+        <Route exact path="/app" element={<App />} />
+      </Route>
     </>
   )
 );
